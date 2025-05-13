f9d3c5d9af65f918cc677c5b6634af96b7705975
pabdu <abdullalipeyman@gmail.com>
2025-05-13 23:18:07 +0400

    first commit

---
+++ src/main/java/az/ingressprojects/IngressProjectsApplication.java
@@ -0,0 +1,13 @@
+package az.ingressprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/ingressprojects/IngressProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingressprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
