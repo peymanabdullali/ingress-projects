f7104a8e77ff3e2843d0593d720f3f7c4041f81f
pabdu <abdullalipeyman@gmail.com>
2025-05-17 10:21:20 +0400

    adding new class: Test.java

---
+++ src/main/java/az/ingressprojects/Test.java
@@ -0,0 +1,8 @@
+package az.ingressprojects;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("I'm java backend developer");
+    }
+}
